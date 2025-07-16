b280a25883405562f1752325234a3c376a0d5e3f
sanbedoyau <sanbedoya@unal.edu.co>
2025-07-15 21:58:46 -0500

    FIX: import .js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCategory, getAllCategories, editCategory, deleteCategory } from '../controllers/category';
+import { createCategory, getAllCategories, editCategory, deleteCategory } from '../controllers/category.js';
 import { verifyToken, onlyAdmin } from '../middlewares/auth.js';
 import { validateIdParam } from '../middlewares/validateParams.js';
 
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { createInventoryItem, getAllInventoryItems, editInventoryItem, deleteInventoryItem } from '../controllers/inventory';
-import { verifyToken, onlyAdmin } from '../middlewares/auth';
-import { validateIdParam } from '../middlewares/validateParams';
+import { createInventoryItem, getAllInventoryItems, editInventoryItem, deleteInventoryItem } from '../controllers/inventory.js';
+import { verifyToken, onlyAdmin } from '../middlewares/auth.js';
+import { validateIdParam } from '../middlewares/validateParams.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, onlyAdmin, createInventoryItem);
 router.get('/', verifyToken, getAllInventoryItems);
-router.put('/:id', verifyToken, onlyAdmin, editInventoryItem);
-router.delete('/:id', verifyToken, onlyAdmin, deleteInventoryItem);
+router.put('/:id', verifyToken, onlyAdmin, validateIdParam, editInventoryItem);
+router.delete('/:id', verifyToken, onlyAdmin, validateIdParam, deleteInventoryItem);
 
 export default router;
